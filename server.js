bf72cc6da519424c12573c826a22621324cfe571
ForestDr <nikita2001395@gmail.com>
2023-06-20 20:03:16 +0300

    fix

--- server.js
+++ server.js
@@ -4,10 +4,6 @@ const app = require("./app");
 
 const { DB_HOST, PORT = 8000 } = process.env;
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000");
-});
-
 mongoose
   .connect(DB_HOST)
   .then(() => {
