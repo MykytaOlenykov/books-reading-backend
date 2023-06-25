f70137e13cd19858f0dc7537099660db3f243f9c
ForestDr <nikita2001395@gmail.com>
2023-06-25 15:37:33 +0300

    fix

--- app.js
+++ app.js
@@ -11,12 +11,12 @@ const app = express();
 
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 
-const { CLIENT_URL, SECRET_COOKIE_KEY } = process.env;
+const { CLIENT_URL, COOKIE_SECRET_KEY } = process.env;
 
 app.use(logger(formatsLogger));
 app.use(express.json());
 app.use(cors({ credentials: true, origin: CLIENT_URL }));
-app.use(cookieParser(SECRET_COOKIE_KEY));
+app.use(cookieParser(COOKIE_SECRET_KEY));
 
 app.use("/api/users", usersRouter);
 app.use("/api/books", booksRouter);
