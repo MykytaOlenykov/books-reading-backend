16e51660a33b3244fc97d2e4378bd686c97c0a67
ForestDr <nikita2001395@gmail.com>
2023-07-22 13:30:50 +0300

    fix

--- controllers/books/deleteById.js
+++ controllers/books/deleteById.js
@@ -1,13 +1,21 @@
 const { Book } = require("../../models/book");
+const { Plan } = require("../../models/plan");
 const { HttpError } = require("../../helpers");
 
 const deleteById = async (req, res) => {
+  const { _id: owner } = req.user;
   const { bookId } = req.params;
 
   const result = await Book.findByIdAndRemove(bookId);
 
   if (!result) {
     throw HttpError(404);
+  }
+
+  const plan = await Plan.findOne({ owner, books: { $in: [bookId] } });
+
+  if (plan) {
+    throw HttpError(400, "This book is included in the plan");
   }
 
   res.json({
