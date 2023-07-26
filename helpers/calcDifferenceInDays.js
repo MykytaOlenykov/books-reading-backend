9bd868d8ec74f3380c3b14ca47f8696351e53b9a
ForestDr <nikita2001395@gmail.com>
2023-07-26 12:49:22 +0300

    fix calc time

--- helpers/calcDifferenceInDays.js
+++ helpers/calcDifferenceInDays.js
@@ -5,6 +5,9 @@ const calcDifferenceInDays = (startDate, endDate, timezone) => {
   const startDateUtc = zonedTimeToUtc(startDate, timezone);
   const endDateUtc = zonedTimeToUtc(endDate, timezone);
 
+  console.log("startDateUtc", startDateUtc);
+  console.log("endDateUtc", endDateUtc);
+
   return differenceInDays(endDateUtc, startDateUtc);
 };
 
